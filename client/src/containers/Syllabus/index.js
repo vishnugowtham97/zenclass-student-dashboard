7935b6f3176be5e8336d9096f7cc82c69dc369ae
vishnugowtham97 <vishnugowtham97@gmail.com>
2023-06-20 13:12:28 +0530

    classname updated

--- client/src/containers/Syllabus/index.js
+++ client/src/containers/Syllabus/index.js
@@ -3,13 +3,13 @@ import React from "react";
 export function Syllabus() {
   return (
     <>
-      <div class="col d-flex justify-content-center">
+      <div className="col d-flex justify-content-center">
         <h1 className="heading">Webcode</h1>
-        <div class="card" style={{ marginTop: "20px" }}>
-          <div class="card-body">
+        <div className="card" style={{ marginTop: "20px" }}>
+          <div className="card-body">
             <hr className="sidebar-divider my-4" />
             <h6
-              class="card-subtitle mb-2 text-muted"
+              className="card-subtitle mb-2 text-muted"
               style={{ fontSize: "25px" }}
             >
               FSD MERN -<a href="/download/mern-syllabus.pdf">Download</a>
